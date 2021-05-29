1aa8d60d75360c88cedbc997697eda87a31f8b64
ThanhTon115 <thanhtontran115@gmail.com>
2021-05-30 04:01:37 +0700

    ghi danh done

--- app/src/main/java/com/example/androidfeedback/MainActivity.java
+++ app/src/main/java/com/example/androidfeedback/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
